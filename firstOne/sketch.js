6fb78b25914c2b38f654051ec93a133dc8d652ed
Sean Kirkpatrick <mbp@SKMacBook.nyc.rr.com>
2018-01-20 01:25:18 -0500

    Added fill color options

--- firstOne/sketch.js
+++ firstOne/sketch.js
@@ -1,8 +1,10 @@
 function setup() {
-  createCanvas(500,500);
+  createCanvas(1400,800);
   background(150);
 }
 
 function draw() {
+  fill(mouseX * .175, mouseY * .3, 150);
+  ellipse(mouseX, mouseY, 250, 250);
 
 }
