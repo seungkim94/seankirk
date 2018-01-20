41dce43dc3cde3e1ed097150eda95c1148284f36
Sean Kirkpatrick <mbp@SKMacBook.nyc.rr.com>
2018-01-20 01:58:04 -0500

    Added strokeWeight and noCursor

--- firstOne/sketch.js
+++ firstOne/sketch.js
@@ -1,10 +1,12 @@
 function setup() {
   createCanvas(1400,800);
-  background(150);
+  background(0);
+  noCursor();
 }
 
 function draw() {
   fill(mouseX * .175, mouseY * .3, 150);
   ellipse(mouseX, mouseY, 250, 250);
+  strokeWeight(mouseX)
 
 }
